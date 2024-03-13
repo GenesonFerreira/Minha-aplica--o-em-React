2d225475b0e209a2deb42f92c541ddff2d76d425
GenesonFerreira <jgeneson@gmail.com>
2024-03-13 16:25:11 -0300

    importação e aplicação de icones para ultilização v-1 atualização de icones no pacote json v-1 Aplicação de elementos e classes da aplicação -v2 importação de imagem de logo -v1 importação de imagem para backgroud -v1 atualização de codigo -v2 aplicação de css e funcionalidades -v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import LogoIMG from './assets/Logo-car.png'
+import { FaUser, FaLock } from 'react-icons/fa';
+import './syles.css'
 
 function App() {
   return (
-    <h1>Olá Mundo</h1>
-  );
+      <div className="container">
+        <div className="container-login">
+          <div className="wrap-login">
+            <form className="login-form">
+
+              <span className="login-form-title">
+                <img src={LogoIMG} alt="Logo-car" />
+              </span>
+
+              <div className="wrap-input">
+                <FaUser className="icon"/>
+                <input className="input" type="email" placeholder='USERNAME' />
+              </div>
+
+              <div className="wrap-input">
+                <FaLock className="icon"/>
+                <input className="input" type="password" placeholder='PASSWORD' />
+              </div>
+
+              <div className="container-login-form-btn">
+                <button className="login-form-btn">LOGIN</button>
+              </div>
+
+              <div className="txt-center">
+                <span className="txt1">Não possui conta?</span>
+
+                <a className='txt2' href="#">Criar conta</a>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default App;
